f30f11b08a3fa205ffc1f425342e19a2307d2f00
Anafuwecal <anafuwecalebifeanyi@gmail.com>
2026-09-07 16:19:28 +0100

    Cors origin fix

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://localhost:5173', 'https://ed-workflow-app.vercel.app'], 
+  origin: ['http://localhost:5173', 'https://localhost:5173', 'https://ed-eight-blue.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
